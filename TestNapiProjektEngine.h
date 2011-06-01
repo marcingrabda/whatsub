cc9fe6eb9b5bdd12bdbeada065584f0003bc6707
Marcin Grabda <marcin.grabda@gmail.com>
2011-06-01 16:11:57 +0200

    unit tests configured for the project

---
+++ TestNapiProjektEngine.h
@@ -0,0 +1,17 @@
+//
+//  TestNapiProjektEngine.h
+//  WhatSub
+//
+//  Created by Marcin Grabda on 6/1/11.
+//  Copyright 2011 burningtomato.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TestNapiProjektEngine : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ TestNapiProjektEngine.m
@@ -0,0 +1,33 @@
+//
+//  TestNapiProjektEngine.m
+//  WhatSub
+//
+//  Created by Marcin Grabda on 6/1/11.
+//  Copyright 2011 burningtomato.com. All rights reserved.
+//
+
+#import "TestNapiProjektEngine.h"
+
+
+@implementation TestNapiProjektEngine
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testNPFDigest
+{
+    
+}
+
+@end
